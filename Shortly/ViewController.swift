3c86e8adbcfd59192edb4103f32f89c919da920f
absessive <absessive@gmail.com>
2015-02-28 22:45:37 -0600

    Add project

--- Shortly/ViewController.swift
+++ Shortly/ViewController.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate,
+                                        NSURLConnectionDelegate,
+                                        NSURLConnectionDataDelegate {
+
+    @IBOutlet var urlField: UITextField!
+    @IBOutlet var webView: UIWebView!
+    @IBOutlet var shortenButton: UIBarButtonItem!
+    @IBOutlet var shortLabel: UIBarButtonItem!
+    @IBOutlet var clipboardButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +27,88 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func loadLocation( AnyObject ) {
+        var urlText = urlField.text
+        
+        if !urlText.hasPrefix("http:") && !urlText.hasPrefix("https:") {
+            if !urlText.hasPrefix("//") {
+                urlText = "//" + urlText
+            }
+            urlText = "http:" + urlText
+        }
+        
+        let url = NSURL(string: urlText)!
+        webView.loadRequest(NSURLRequest(URL: url))
+    }
+    
+//MARK: URL Shortening
+    
+    let GoDaddyAccountKey = "c6626f8fc21c49acbfa14ff897d4feb9"
+    var shortenURLConnection: NSURLConnection?
+    var shortURLData: NSMutableData?
+    
+    @IBAction func shortenURL( AnyObject ) {
+        if let toShorten = webView.request?.URL.absoluteString {
+//            let encodedURL = toShorten.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+            let encodedURL = toShorten
+            let urlString = "http://api.x.co/Squeeze.svc/text/\(GoDaddyAccountKey)?url=\(encodedURL)"
+            shortURLData = NSMutableData()
+            let request = NSURLRequest(URL:NSURL(string:urlString)!)
+            shortenURLConnection = NSURLConnection(request:request, delegate:self)
+            shortenButton.enabled = false
+        }
+    }
+    
+    @IBAction func clipboardURL( AnyObject ) {
+        let shortURLString = shortLabel.title
+        let shortURL = NSURL(string: shortURLString!)
+        UIPasteboard.generalPasteboard().URL = shortURL
+    }
 
+//MARK: NSURLConnectionDelegate
+    
+    func connection( connection: NSURLConnection!, didFailWithError error: NSError! ) {
+            shortLabel.title = "failed"
+            clipboardButton.enabled = false
+            shortenButton.enabled = true
+    }
+    
+    func connection( connection: NSURLConnection!, didReceiveData data: NSData! ) {
+        shortURLData?.appendData(data)
+    }
+    
+    func connectionDidFinishLoading( connection: NSURLConnection! ) {
+        if let data = shortURLData {
+            let shortURLString = NSString(data:data, encoding:NSUTF8StringEncoding)
+            shortLabel.title = shortURLString
+            clipboardButton.enabled = true
+        }
+    }
 
+//MARK: - UIWebViewDelegate
+    
+    func webViewDidStartLoad( UIWebView ) {
+        shortenButton.enabled = false
+    }
+
+    func webViewDidFinishLoad( UIWebView ) {
+        shortenButton.enabled = true
+        urlField.text = webView.request?.URL.absoluteString
+    }
+    
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError) {
+        var message = "That page could not be loaded. " +
+                        error.localizedDescription
+        let alert = UIAlertController(title: "Could not load URL",
+                                    message: message,
+                             preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "That's sad",
+                                     style: .Default,
+                                   handler: nil)
+        
+        alert.addAction(okAction)
+        presentViewController(alert, animated: true, completion: nil)
+    }
 }
 
